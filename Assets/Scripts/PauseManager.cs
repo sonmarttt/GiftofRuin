5a8ff040920f140746322fa53c9ff9e852c26621
AaronHTF <ajhotimfat@gmail.com>
2026-05-13 09:05:24 -0400

    Added third person camera to all the levels and added pause menu

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuPanel;
+
+    private bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) {
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame() {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame() {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void MainMenu() {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    public void Restart() {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
